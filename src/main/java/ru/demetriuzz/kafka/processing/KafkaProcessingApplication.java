c764426dcc6c3749fe11ec8643559fe1b88d9751
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2021-11-02 11:41:30 +0500

    каркас приложения

---
+++ src/main/java/ru/demetriuzz/kafka/processing/KafkaProcessingApplication.java
@@ -0,0 +1,13 @@
+package ru.demetriuzz.kafka.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProcessingApplication.class, args);
+    }
+
+}
